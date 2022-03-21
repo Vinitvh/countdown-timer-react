c0f7eff75e69a9429b84da89ebaa3051f7c58f5d
Vinit Hemadri <vineet.vh@gmail.com>
2022-03-21 13:41:12 +0530

    Created timer layout

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,31 @@
 function Timer() {
-  return <div>Timer</div>;
+  return (
+    <section className="timer__container">
+      <div>
+        <p>
+          <span className="circle__left"></span> 08
+        </p>
+        <p>days</p>
+      </div>
+      <div>
+        <p>
+          <span className="circle__left"></span> 23
+        </p>
+        <p>hours</p>
+      </div>
+      <div>
+        <p>
+          <span className="circle__left"></span>55
+        </p>
+        <p>minutes</p>
+      </div>
+      <div>
+        <p>
+          <span className="circle__left"></span>49
+        </p>
+        <p>seconds</p>
+      </div>
+    </section>
+  );
 }
 export default Timer;
